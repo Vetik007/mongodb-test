75228f4a5b6218287657071c1bdc8ad981d60b26
Vetik007 <boy0007@ukr.net>
2023-08-11 18:02:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose"); // імпортуємо mongoose
 const app = require("./app");
-// const { DB_HOST } = require("./config");
 
 mongoose.set("strictQuery", true);
 
 // console.log(process.env);
 
-// const DB_HOST =
-//   "mongodb+srv://Vitalii:9L4E74zNJp6pvZKP@cluster0.qttkxlb.mongodb.net/db-contacts?retryWrites=true&w=majority"; // строка підключення до бази яку беремо з MongoDB
+const DB_HOST =
+  "mongodb+srv://Vitalii:9L4E74zNJp6pvZKP@cluster0.qttkxlb.mongodb.net/db-contacts?retryWrites=true&w=majority"; // строка підключення до бази яку беремо з MongoDB
 
 // const DB_HOST = process.env;
 
