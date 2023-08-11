8201bd36d16beb14381bbcc16f9dac6c22ef357f
Vetik007 <boy0007@ukr.net>
2023-08-11 17:01:36 +0300

    add contacts

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
+const mongoose = require("mongoose"); // імпортуємо mongoose
 const app = require("./app");
+const { DB_HOST } = require("./config");
 
-// запускаємо сервер. Першим агументом зазначємо порт на якому буде запущений сервер, другим аргуметом передаємо колбек-функцію яка повертає повідомлення що сервер запущений.
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+mongoose.set("strictQuery", true);
+
+// console.log(process.env);
+
+// const DB_HOST =
+//   "mongodb+srv://Vitalii:9L4E74zNJp6pvZKP@cluster0.qttkxlb.mongodb.net/db-contacts?retryWrites=true&w=majority"; // строка підключення до бази яку беремо з MongoDB
+
+// const DB_HOST = process.env;
+
+// метод connect повертає проміс який треба обробити через then/catch
+mongoose
+  .connect(DB_HOST) // викликаємо метод connect для підключення до бази даних
+  .then(() => {
+    // запускаємо сервер. Першим агументом зазначємо порт на якому буде запущений сервер, другим аргуметом передаємо колбек-функцію яка повертає повідомлення що сервер запущений.
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1); // закриваємо запущені процеси
+  });
